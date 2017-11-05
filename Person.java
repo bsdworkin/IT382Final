998e24e9f6f76224cfa8ef93343bd425109dc9d2
Ben Dworkin <bsdwork@ilstu.edu>
2017-11-05 13:44:55 -0600

    First Commit

---
+++ Person.java
@@ -0,0 +1,15 @@
+//Yo like how do you connect java files with out using an SDK lol 
+
+import java.io.*;
+
+class Person{
+  
+  private ArrayList<String> head;
+  private ArrayList<String> torso;
+  private ArrayList<String> legs;
+
+  public Person(){}
+
+  //TODO rest of implementation of the class
+
+}
